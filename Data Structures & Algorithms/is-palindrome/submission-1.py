8d749d75ed8c505dd16dcac1fa312fe4e768df67
Rycca <126682794+Zimgo012@users.noreply.github.com>
2026-06-11 11:35:42 -0400

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        # trying shortcut for valid palindrome
+        s = ''.join(char.lower() for char in s if char.isalnum())
+        return True if (s == s[::-1]) else False
