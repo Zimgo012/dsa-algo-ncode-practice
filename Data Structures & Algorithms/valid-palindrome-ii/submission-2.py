e42c735b44d48563f1b3462bee96971517542711
Rycca <126682794+Zimgo012@users.noreply.github.com>
2026-06-12 12:50:38 -0400

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        
+      
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            if s[i] != s[j]:
+                skipL, skipR = s[i + 1 : j + 1], s[i : j]
+                return (skipL == skipL[::-1] or skipR == skipR[::-1])
+            i += 1
+            j -=1
+        return True 
